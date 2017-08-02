93ba1468f82df088d1c790afe059bd8fd77f3c9b
AgentBoo <zuzi.han@gmail.com>
2017-08-02 01:28:32 -0400

    added green border, changes to display: in .css

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-// define constant that holds the content of <main> in index.html
-// as template literal
-// each person is a div: name, email, street, city, state, zip, number, ssn
 // start with one individual: customers.results[0]
+// define constant that holds the content of <main> in index.html
+// to be template literal
+// each person is a div: name, email, street, city, state, zip, number, ssn
+
 
 // Grab html element that I will return content to
 const main_content = document.getElementById("main_content")
@@ -11,25 +12,25 @@ const main_content = document.getElementById("main_content")
 for (let i = 0; i < customers.results.length; i++){
 
 // each time, I have to create a new div that I populate with more html content
-// this will create a div inside a div, but I don't really care at this point
-let customerDIV = document.createElement("div");
-customerDIV.classList.add("c-indiv", "f-indiv", "s-indiv")
-let customerContent =
+let empDIV = document.createElement("div");
+empDIV.classList.add("c-indiv", "f-indiv", "s-indiv")
+
+const ppldata = customers.results[i];
+
+let empContent =
 `
-  <img src=${customers.results[i].picture.large} class="ctmImg">
-  <h4 class="ctmName"> ${customers.results[i].name.first} ${customers.results[i].name.last}</h4>
-  <p class="ctmEmail"> ${customers.results[i].email}</p>
-  <p class="ctmLocation"> ${customers.results[i].location.street}</p>
-  <span class="ctmLocation"> ${customers.results[i].location.city} </span>
-  <span class="ctmLocation"> ${customers.results[i].location.state}</span>
-  <span class="ctmLocation"> ${customers.results[i].location.postcode}</span>
-  <p class="ctmLocation"> ${customers.results[i].phone} </p>
-  <p class="ctmSSN"> ${customers.results[i].id.value} </p>
+  <img src=${ppldata.picture.large} class="empImg">
+  <h4 class="empName"> ${ppldata.name.first} ${ppldata.name.last}</h4>
+  <p class="empEmail"> ${ppldata.email}</p>
+  <p class="empLocation"> ${ppldata.location.street}</p>
+  <p class="empLocation"> ${ppldata.location.city}, ${ppldata.location.state} ${ppldata.location.postcode}</p>
+  <p class="empPhone empLocation"> ${ppldata.phone} </p>
+  <p class="empSSN"> ${ppldata.id.value} </p>
 `
 // Put everything back in the div
-customerDIV.innerHTML = customerContent;
+empDIV.innerHTML = empContent;
 // return the div in the <main> every time
-main_content.appendChild(customerDIV);
+main_content.appendChild(empDIV);
 }
 
 // Hover using javascript
